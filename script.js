a34bf3ab8c29550ba77d363bda65549d662f8094
Pooja Jadhav <pgjadhav603@gmail.com>
2025-09-25 10:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 //your JS code here. If required.
-const container = document.getElementById("board");
+ const container = document.getElementById("board");
 
+    // Create 800 squares
+    for (let i = 0; i < 800; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
 
-// Create 800 squares
-for (let i = 0; i < 800; i++) {
-const square = document.createElement("div");
-square.classList.add("square");
+      square.addEventListener("mouseover", () => {
+        square.classList.add("hovered");
 
+        // Reset color after 1s
+        setTimeout(() => {
+          square.classList.remove("hovered");
+        }, 1000);
+      });
 
-square.addEventListener("mouseover", () => {
-const randomColor = `hsl(${Math.random() * 360}, 70%, 60%)`;
-square.style.backgroundColor = randomColor;
-
-
-// Reset color after 1s
-setTimeout(() => {
-square.style.backgroundColor = "#eee";
-}, 1000);
-});
-
-
-container.appendChild(square);
-}
+      container.appendChild(square);
+    }
