5e3a4bdf0fafa40f551380a35c6c49ce2529d2e6
Pooja Jadhav <pgjadhav603@gmail.com>
2026-01-16 14:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-<script>
-  const container = document.getElementById("board");
-  const BOXES = 800;
+const container = document.getElementById("board");
+    const BOXES = 800;
 
-  for (let i = 0; i < BOXES; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+    for (let i = 0; i < BOXES; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
 
-    let timer;
+      square.addEventListener("mouseenter", () => {
+        const color = randomColor();
+        square.style.backgroundColor = color;
 
-    square.addEventListener("mouseover", () => {
-      clearTimeout(timer);
-      square.style.backgroundColor = randomColor();
-    });
+        setTimeout(() => {
+          square.style.backgroundColor = "#1d1d1d";
+        }, 1000);
+      });
 
-    square.addEventListener("mouseout", () => {
-      timer = setTimeout(() => {
-        square.style.backgroundColor = "#1d1d1d";
-      }, 1000);
-    });
+      container.appendChild(square);
+    }
 
-    container.appendChild(square);
-  }
-
-  function randomColor() {
-    const colors = [
-      "#e74c3c",
-      "#8e44ad",
-      "#3498db",
-      "#e67e22",
-      "#2ecc71",
-      "#f1c40f"
-    ];
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-</script>
+    function randomColor() {
+      const colors = [
+        "#e74c3c",
+        "#8e44ad",
+        "#3498db",
+        "#e67e22",
+        "#2ecc71",
+        "#f1c40f"
+      ];
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
